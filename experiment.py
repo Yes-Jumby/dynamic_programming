431bd2c472e7d16cb49544104d53e60c57393c62
Yes-Jumby <51182133+Yes-Jumby@users.noreply.github.com>
2023-01-09 14:33:44 +0800

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -5,6 +5,53 @@ import CuttingPlane
 import MonteCarlo
 import HungarianAssignment
 
+def branch_and_bound_test_my():
+    print("\nBranch and Bound Test:\n" + "-" * 22)
+    c = [16, 10, 12,15,14,17,6,8,10,7,11,11,4,5,12]
+    A_eq = [[1, 1, 1,0,0,0,0,0,0,0,0,0,0,0,0], 
+            [0, 0, 0,1,1,1,0,0,0,0,0,0,0,0,0], 
+            [0, 0, 0,0,0,0,1,0,0,1,0,0,1,0,0], 
+            [0, 0, 0,0,0,0,0,1,0,0,1,0,0,1,0], 
+            [0, 0, 0,0,0,0,0,0,1,0,0,1,0,0,1]]
+    b_eq = [300, 300,200,100,300]
+    bounds = [(0, None), (0, None), (0, None),
+              (0, None), (0, None), (0, None),
+              (0, None), (0, None), (0, None),
+              (0, None), (0, None), (0, None),
+              (0, None), (0, None), (0, None)]
+    tree = BranchAndBound.BnBTree()
+    r = BranchAndBound.branch_and_bound(c, None, None, A_eq, b_eq, bounds, tree.root)
+    print(r)
+    print(tree)
+#0-1背包
+#音响 4-3000 x1
+#笔记本电脑 3-2000 x2
+#笔记本电脑 1-1500 x3
+#总重量 4
+#x1\2\3 <=1
+#x1*4+x2*3+x3*1<=4
+#3000*x1+2000*x2+1500*x3
+def branch_and_bound_test_01():
+    print("\nBranch and Bound Test:\n" + "-" * 22)
+    c = [-3000,-2000,-1500]
+    # A_up = [[4, 3, 1], 
+    #         [1, 0, 0], 
+    #         [0, 1, 0], 
+    #         [0, 0, 1]]
+    # b_up = [4, 1,1,1]
+    # bounds = [(0, None), (0, None), (0, None)]
+    A_up = [[4, 3, 1]]
+    b_up = [4]
+    bounds = [(0, 1), (0, 1), (0, 1)]
+    
+    # #多重背包
+    # A_up = [[4, 3, 1]]
+    # b_up = [4]
+    # bounds = None
+    tree = BranchAndBound.BnBTree()
+    r = BranchAndBound.branch_and_bound(c, A_up, b_up, None, None, bounds, tree.root)
+    print(r)
+    print(tree)
 
 def branch_and_bound_test():
     print("\nBranch and Bound Test:\n" + "-" * 22)
@@ -55,7 +102,9 @@ def hungarian_assignment_test():
 
 
 if __name__ == "__main__":
-    branch_and_bound_test()
-    cutting_plane_test()
-    monte_carlo_test()
-    hungarian_assignment_test()
+    # branch_and_bound_test()
+    # cutting_plane_test()
+    # monte_carlo_test()
+    # hungarian_assignment_test()
+    # branch_and_bound_test_my()
+    branch_and_bound_test_01()
